033cea70f33299ed1c2db0e6760ca6252110dd39
lorem.bss@gmail.com <2584563719Vlad>
2022-12-30 21:28:58 +0100

    Create ImageController

---
+++ src/main/java/com/webmuseum/museum/controllers/ImageController.java
@@ -0,0 +1,30 @@
+package com.webmuseum.museum.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.webmuseum.museum.service.IStorageService;
+
+@Controller
+@RequestMapping("image")
+public class ImageController {
+
+    @Autowired
+    private IStorageService storageService;
+
+    
+    @GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> getImage(@PathVariable String filename) {
+      Resource file = storageService.loadAsResource(filename);
+  
+      return ResponseEntity.ok()
+          .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+    
+}
