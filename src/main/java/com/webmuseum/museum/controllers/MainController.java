c2bc7cc239cdbf87eb0fbc11be1d628c7fb8b26d
lorem.bss@gmail.com <2584563719Vlad>
2023-01-07 00:52:43 +0100

    Refactoring - remove unused imports

--- src/main/java/com/webmuseum/museum/controllers/MainController.java
+++ src/main/java/com/webmuseum/museum/controllers/MainController.java
@@ -1,7 +1,5 @@
 package com.webmuseum.museum.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.webmuseum.museum.dto.ExhibitViewDto;
-import com.webmuseum.museum.entity.Exhibit;
 import com.webmuseum.museum.service.IExhibitService;
 
 @Controller
